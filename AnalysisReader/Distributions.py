5e8f9f8e1cc1924f2a3f84c9ab01fd6511c2a807
pabloferm <pablo.fernandez@dipc.org>
2022-11-28 10:00:07 +0100

    Motivation for non-Gaussian priors

--- AnalysisReader/Distributions.py
+++ AnalysisReader/Distributions.py
@@ -1,6 +1,20 @@
-# Provides parameters of non-Gaussian probability distributions
+# Provides several functions for probability distributions
+from math import gamma, exp, sqrt, pi, log
 
-def Beta(m,s):
+def BetaPar(m,s):
     alpha = ((1-m)/s**2-1/m)*m**2
     beta = alpha*(1/m-1)
     return alpha,beta
+
+def Beta(x,a,b):
+    return x**(a) * (1-x)**(b) * gamma(a+b) / gamma(a) / gamma(b)
+
+def Gaussian(x,m,s):
+    return exp(-0.5 * (x - m)**2 / s**2) / (s*sqrt(2*pi)) 
+
+def logGaussianPrior(x,m,s): # Actually, -2 ln(L/L0)
+    return (x-m)**2/s**2
+
+def logBetaPrior(x,m,s):
+    a,b = BetaPar(m,s)
+    return 2 * (a * log(m/x) + b * log((1-m) / (1-x)))
