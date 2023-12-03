14b7109f0ecdb7d2c53549229b62d412f7db6723
pabloferm <pablo.fernandez@dipc.org>
2023-12-03 23:43:39 +0100

    untracked files

---
+++ pynu/adam_minimizer.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+
+def adam_minimizer(
+        f,
+        initial_point,
+        args=None,
+        learning_rate=0.01,
+        beta1=0.9,
+        beta2=0.999,
+        epsilon=1e-8,
+        precission=1e-4,
+        num_iterations=100,
+        bounds=None):
+    # Initialize variables
+    point = initial_point
+    m = np.zeros_like(point)
+    v = np.zeros_like(point)
+    t = 0
+    print(f'Initial point, {point}')
+    if args:
+        X2, gradient = f(point, args)
+        print(X2)
+        print(gradient)
+    else:
+        X2, gradient = f(point)
+    gradient_norm = np.linalg.norm(gradient)
+    print(f'Gradient norm is {gradient_norm}')
+
+    while gradient_norm > precission:
+        # for _ in range(num_iterations):
+        t += 1
+
+        if args:
+            X2, gradient = f(point, args)
+        else:
+            X2, gradient = f(point)
+
+        # Compute first and second moment estimates
+        m = beta1 * m + (1 - beta1) * gradient
+        v = beta2 * v + (1 - beta2) * (gradient ** 2)
+
+        # Bias correction
+        m_hat = m / (1 - beta1 ** t)
+        v_hat = v / (1 - beta2 ** t)
+
+        # Update point
+        print(learning_rate * m_hat / (np.sqrt(v_hat) + epsilon))
+        point -= learning_rate * m_hat / (np.sqrt(v_hat) + epsilon)
+        print(point)
+        if bounds:
+            for i, (p, b) in enumerate(zip(point, bounds)):
+                if p >= b[1]:
+                    point[i] = b[1]
+                    in_bounds = True
+                elif p <= b[0]:
+                    point[i] = b[0]
+                    in_bounds = True
+                else:
+                    in_bounds = False
+        print(f'New point coser to the minimum, {point}')
+        gradient_norm = np.linalg.norm(gradient)
+        if t > num_iterations:
+            print('WARNING: Too many iterations. The precission required may be too low, the number of number of iterations too low, or the minimizer is diverging.')
+            print(f'Gradient norm is {gradient_norm}')
+            return point
+
+    if in_bounds:
+        print('WARNING: minimum at bounds.')
+
+    return point
---
+++ test/linear_descent_minimizer.py
@@ -0,0 +1,54 @@
+import numpy as np
+import time
+
+def linear_gradient_descent_minimizer(fn, gradient_fn, initial_point, precission=1e-4, num_iterations=10000, bounds=None):
+    point = initial_point
+    loop_counter = 0
+    in_bounds = False
+    print(f'Initial point, {point}')
+    gradient = gradient_fn(point)
+    gradient_norm = np.linalg.norm(gradient)
+
+    while gradient_norm > precission:
+        gradient = gradient_fn(point)
+        gradient_norm = np.linalg.norm(gradient)
+        point = point - fn(point) / (gradient + 1e-5)
+        if bounds:
+            for i, (p, b) in enumerate(zip(point, bounds)):
+                if p >= b[1]:
+                    point[i] = b[1]
+                    in_bounds =True
+                elif p <= b[0]:
+                    point[i] = b[0]
+                    in_bounds =True
+        loop_counter += 1
+        print(loop_counter)
+
+        if loop_counter > num_iterations:
+            print('WARNING: Too many iterations. The precission required may be too low, the number of number of iterations too low, or the minimizer is diverging.')
+            print(f'Gradient norm is {gradient_norm}')
+            return point
+    print(f'Achieved minimum in {loop_counter} iterations.')
+    return point
+
+# Example usage:
+def function_to_minimize(x):
+    # Define your function here
+    return x[0]**2  + 3*x[1]**2 + 1
+    # print(r'Example function: $x^2$')
+
+def gradient_function(x):
+    # Calculate the derivative of the function here
+    return np.array([2 * x[0], 6*x[1]])  # Derivative of x^2 is 2x
+
+initial_point = np.array([1.0, 10.0])  # Initial point for the minimization
+bounds = ((-1,2),(-2,11))
+learning_rate = 0.1  # Learning rate determines the step size
+num_iterations = 100  # Number of iterations for gradient descent
+
+start = time.perf_counter()
+minimized_point = linear_gradient_descent_minimizer(function_to_minimize, gradient_function, initial_point, bounds=bounds)
+print(f'It took {time.perf_counter()-start} seconds')
+print(minimized_point)
+minimized_value = function_to_minimize(minimized_point)
+print(minimized_value)
