073f99502fa9ce83753ddd4b45f246b155ebe654
pabloferm <pablo.fernandez@dipc.org>
2024-06-06 05:01:56 +0200

    SuperK Pheno for 2023 data release

--- pynu/Experiments/SuperK_Atm_Pheno.py
+++ pynu/Experiments/SuperK_Atm_Pheno.py
@@ -234,3 +234,170 @@ class SuperK_Gdtag(SuperK_Htag):
         self.Detector = "SuperK_Gdtag_Pheno"
 
         self.Definition()
+
+
+class SuperK_2023(SuperK):
+    def __init__(self, dict_of_details, scenario):
+        super(SuperK_Htag, self).__init__(dict_of_details)
+
+        self.Detector = "SuperK_2023"
+
+        self.Definition()
+
+        self.Binning()
+
+        self.SetBinner_2D()
+
+        if self.DataFit:
+            self.DataVariables()
+            self.BinData()
+
+    def MCVariables(self):
+        d_itype = self.MC["itype"]
+        # condition = d_itype > -1
+        self.EReco = self.MC["evis"]
+        self.CosZReco = self.MC["recodirZ"]
+        self.CosZTrue = self.MC["dirnuZ"]
+        self.AziTrue = self.MC["azi"]
+        self.Mode = self.MC["mode"]
+        self.CC = np.abs(self.Mode) < 30
+        self.nuPDG = self.MC["ipnu"]
+        self.ETrue = self.MC["pnu"]
+        # self.Weight = self.MC["weightReco"] * self.MC["weightSim"]
+        self.Weight = 1 / 50.
+        self.Sample = self.MC["itype"]  # Sample of each event
+        self.DecayE = self.MC["muedk"]
+
+        self.NumberOfEvents = self.Sample.size
+        self.Samples = np.unique(self.Sample)  # Samples in the analysis
+        self.NumberOfSamples = 1 + np.amax(self.Samples)
+        # self.NumberOfSamples = 16
+        self.Erec_max = max(self.EReco)
+        self.Erec_min = min(self.EReco)
+        self.Etrue_min = min(self.ETrue)
+        self.Etrue_max = max(self.ETrue)
+        self.E_edges = [self.Erec_min, self.Erec_max]
+        self.Z_edges = [-1, 1]
+
+        self.NORM *= 1
+
+        self.BaseWeight = self.Weight * self.NORM
+
+        del self.MC
+
+    def DataVariables(self):
+        d_itype = self.Data["itype"]
+        # condition = (d_itype < 16) * (d_itype > -1)
+        self.dEReco = self.Data["evis"]
+        self.dCosZReco = self.Data["recodirZ"]
+        self.dSample = self.Data["itype"]  # Sample of each event
+        # self.dDecayE = self.Data["muedk"]
+        self.dNumberOfEvents = self.Sample.size
+
+        del self.Data
+
+    def Binning(self):
+        sg_ebins = np.array([0.1, 0.25118864315095796, 0.3981071705534973, 0.630957344480193, 1.0, 1.584893192461114])
+        mg_4_ebins = np.array([1.0, 2.5118864315095797, 5.011872336272725, 10.0, 100.0])
+        mg_2_ebins = np.array([1.3, 2.5118864315095797, 100.0])
+        mr_3_ebins = np.array([1.0, 2.5118864315095797, 5.011872336272725, 100.0])
+        mr_4_ebins = np.array([0.1, 1.3299998745408388, 2.5118864315095797, 5.011872336272725, 100.0])
+        pcs_ebins = np.array([0.1, 2.5118864315095797, 100.0])
+        pct_ebins = np.array([0.1, 1.32739445772974, 2.5118864315095797, 5.011872336272725, 100.0])
+        upmus_ebins = np.array([1.584893192461114, 2.4945947269429536, 4.9888448746001215, 100000.0])
+        upmut_ebins = np.array([0.1, 100000.0])
+        z10bins = np.array([-1, -0.839, -0.644, -0.448, -0.224, 0.0, 0.224, 0.448, 0.644, 0.839, 1.0])
+        z10bins_up = np.array([-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0.0])
+        z1bins = np.array([-1, 1.0])
+        self.sample_names = {
+            0: 'sk1-3_fc_subgev_1ring_elike_0decaye',
+            1: 'sk1-3_fc_subgev_1ring_elike_1decaye',
+            2: 'sk1-5_fc_1ring_ncpi0',
+            3: 'sk1-3_fc_subgev_1ring_mulike_0decaye',
+            4: 'sk1-3_fc_subgev_1ring_mulike_1decaye',
+            5: 'sk1-3_fc_subgev_1ring_mulike_2decaye',
+            6: 'sk1-5_fc_2ring_ncpi0',
+            7: 'sk1-3_fc_multigev_1ring_nuelike',
+            8: 'sk1-3_fc_multigev_1ring_nuebarlike',
+            9: 'sk1-3_fc_multigev_1ring_mulike',
+            10: 'sk1-5_fc_multigev_multiring_nuelike',
+            11: 'sk1-5_fc_multigev_multiring_nuebarlike',
+            12: 'sk1-5_fc_multigev_multiring_mulike',
+            13: 'sk1-5_fc_multigev_multiring_other',
+            14: 'sk1-5_pc_stop',
+            15: 'sk1-5_pc_thru',
+            16: 'sk1-5_upmu_stop',
+            17: 'sk1-5_upmu_thru_nonshowering',
+            18: 'sk1-5_upmu_thru_showering',
+            19: 'sk4-5_fc_subgev_1ring_nuelike',
+            20: 'sk4-5_fc_subgev_1ring_nuebarlike_0neutron',
+            21: 'sk4-5_fc_subgev_1ring_nuebarlike_1neutron',
+            22: 'sk4-5_fc_subgev_1ring_numulike',
+            23: 'sk4-5_fc_subgev_1ring_numubarlike',
+            24: 'sk4-5_fc_multigev_1ring_nuelike',
+            25: 'sk4-5_fc_multigev_1ring_nuebarlike_0neutron',
+            26: 'sk4-5_fc_multigev_1ring_nuebarlike_1neutron',
+            27: 'sk4-5_fc_multigev_1ring_numulike',
+            28: 'sk4-5_fc_multigev_1ring_numubarlike'}
+        self.EnergyBins = {
+            0: sg_ebins,
+            1: sg_ebins,
+            2: sg_ebins,
+            3: sg_ebins,
+            4: sg_ebins,
+            5: sg_ebins,
+            6: sg_ebins,
+            7: mg_4_ebins,
+            8: mg_4_ebins,
+            9: mg_2_ebins,
+            10: mr_3_ebins,
+            11: mr_3_ebins,
+            12: mr_4_ebins,
+            13: mg_4_ebins,
+            14: pcs_ebins,
+            15: pct_ebins,
+            16: upmus_ebins,
+            17: upmut_ebins,
+            18: upmut_ebins,
+            19: sg_ebins,
+            20: sg_ebins,
+            21: sg_ebins,
+            22: sg_ebins,
+            23: sg_ebins,
+            24: mg_4_ebins,
+            25: mg_4_ebins,
+            26: mg_4_ebins,
+            27: mg_2_ebins,
+            28: mg_2_ebins
+        }
+        self.CzBins = {
+            0: z10bins,
+            1: z1bins,
+            2: z1bins,
+            3: z10bins,
+            4: z10bins,
+            5: z1bins,
+            6: z1bins,
+            7: z10bins,
+            8: z10bins,
+            9: z10bins,
+            10: z10bins,
+            11: z10bins,
+            12: z10bins,
+            13: z10bins,
+            14: z10bins,
+            15: z10bins,
+            16: z10bins_up,
+            17: z10bins_up,
+            18: z10bins_up,
+            19: z10bins,
+            20: z10bins,
+            21: z10bins,
+            22: z10bins,
+            23: z10bins,
+            24: z10bins,
+            25: z10bins,
+            26: z10bins,
+            27: z10bins,
+            28: z10bins
+        }
